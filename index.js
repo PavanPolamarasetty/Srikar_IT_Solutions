167b2e1bd082f97d789e5c182f951b87188d68b4
pavan venkata sai <Pavan>
2020-11-11 03:32:26 +0530

    Form Validation Changes in passwords and buttons

--- index.js
+++ index.js
@@ -1,35 +1,70 @@
-const togglePassword=document.getElementById("toggle");
+const toggle0=document.getElementById("toggle0");
+const toggle1=document.getElementById("toggle1");
+const toggle2=document.getElementById("toggle2");
 const password=document.getElementById("password");
 const check=document.getElementById("check");
 const button=document.getElementById("button");
 const form1=document.getElementById("form1");
-togglePassword.addEventListener('click',function(e){
-
-    const type=password.getAttribute('type')==='password'?'text':'password';
-
-    
-    password.setAttribute('type',type);
-
-    this.classList.toggle('fa-eye-slash');
-
-});
-
 const confirmPassword=document.getElementById("confirmPassword");
-
-
-button.addEventListener('click',function () {
+const loginPassword=document.getElementById("loginPassword");
+const   mail=document.getElementById("mail");
+const loginButton=document.getElementById("loginButton");
+function toggle(d)
+{
     
-        button.setAttribute('type','button');
-        if (password.value!= confirmPassword.value) {
-            
-            
-            check.textContent = "Passwords donot Match!!!";
-            
+
+    const fields=[password,confirmPassword,loginPassword];    
+        const type=fields[d].type=="password"?"text":"password";
+        fields[d].setAttribute('type',type);
+        
+        const eyefields=[toggle0,toggle1,toggle2];
+        
+              eyefields[d].classList.toggle('fa-eye-slash');
+        
+
+}
+
+
+
+
+
+
+var checkit=function ()
+{
+    if(confirmPassword.value.length>0)
+    {
+        if(password.value!=confirmPassword.value)
+        {
+            check.innerHTML="Passwords donot Match!!!";
         }
         else
         {
-            button.setAttribute('type','submit');
-            form1.setAttribute('action',"regsuccess.html");
-
+                 check.innerHTML="Passwords Matching!";
+                check.style.color='green';  
+            
         }
-    });
+    }
+    
+}
+
+if(mail.value.length==0|| loginPassword.value.length==0)
+{
+    loginButton.style.backgroundImage="linear-gradient(45deg, grey, transparent)";
+    loginButton.style.backgroundColor="grey"
+    loginButton.disabled=true;
+}
+function buttonCheck()
+{
+    if(mail.value.length==0|| loginPassword.value.length==0)
+{
+    loginButton.style.backgroundImage="linear-gradient(45deg, grey, transparent)";
+    loginButton.style.backgroundColor="grey"
+    loginButton.disabled=true;
+}
+else
+{
+    loginButton.style.backgroundImage=" -webkit-linear-gradient(top, #3D94F6, #1E62D0)";
+    loginButton.style.backgroundColor="blue"
+    loginButton.disabled=false;
+}
+}
